80d74d846b3e9447d41910d6af9e3c4076424144
Katarzyna Kanicka <01187237@pw.edu.pl>
2024-10-15 11:07:56 +0200

    updated the gradient descent function

--- lab1/gradient_descent.py
+++ lab1/gradient_descent.py
@@ -1,23 +1,28 @@
 import numpy as np
 from matplotlib import pyplot as plt
-import math
 
 
 def f(x):
-    return 8*x + 8*math.sin(x)
+    return 8*x + 8*np.sin(x)
 
 def deriv_f(x):
-    return 8 + 8*math.cos(x)
+    return 8 + 8*np.cos(x)
 
-def gradient_descent(start, gradient, step_length, limit):
+def gradient_descent(start, gradient, step_length, limit, domain):
     x = start
     points = [x]
     steps = 0
-    while steps < limit:
+    while steps < limit and domain.min() < x < domain.max():
         d = gradient(x)
         x = x - step_length * d
         points.append(x)
         steps += 1
-    return points
+    return np.array(points)
 
+xs = np.linspace(-4 * np.pi, 4 * np.pi)
+points = gradient_descent(0.4, deriv_f, 0.1, 10, xs)
+plt.plot(xs, f(xs))
+plt.plot(points, f(points), '-o')
+plt.show()
 
+# print(gradient_descent(0, deriv_f, 0.2, 40, xes))
