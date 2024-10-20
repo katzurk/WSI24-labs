efa554b4b53475e539519b5c6698fbf248e7c730
Katarzyna Kanicka <01187237@pw.edu.pl>
2024-10-20 21:10:45 +0200

    updated the code structure and refactorized the code

--- lab1/main_gd.py
+++ lab1/main_gd.py
@@ -1,6 +1,8 @@
-from gradient_descent import gradient_descent, deriv_f, deriv_g, plot_f, plot_g
+from gradient_descent import *
+from run_tests import *
 import numpy as np
 import argparse
+import os
 
 
 def parse_arguments():
@@ -10,24 +12,31 @@ def parse_arguments():
     parser.add_argument("step_length", type=float)
     parser.add_argument("--step_limit", type=int, nargs='?', default=1000000)
     parser.add_argument("--visualize", action="store_true")
-
+    parser.add_argument("--run_tests", action="store_true")
 
     args = parser.parse_args()
-    function = True if args.function == "f" else False
-    deriv_function = deriv_f if function else deriv_g
-    domain = np.linspace(-4 * np.pi, 4 * np.pi) if function else np.linspace(-2, 2)
-    points, steps = gradient_descent(
-        args.start_point, args.step_length, deriv_function, domain, args.step_limit
-    )
+    domain = np.linspace(-4 * np.pi, 4 * np.pi) if args.function == "f" else np.linspace(-2, 2)
+
+    if args.run_tests:
+        main_dir = f"function_{args.function}"
+        directories = [f"{main_dir}/starting_point", f"{main_dir}/step_length", f"{main_dir}/iterations_limit"]
+        for directory in directories:
+            os.makedirs(directory, exist_ok=True)
+        run_tests_gradient_descent(args.start_point, args.function, args.step_length, domain, args.step_limit)
 
     if args.visualize:
-        if function:
+        deriv_function = deriv_f if args.function == "f" else deriv_g
+        points, steps = gradient_descent(
+            args.start_point, args.step_length, deriv_function, domain, args.step_limit
+        )
+        if args.function == "f":
             plot_f(points)
         else:
             plot_g(points)
 
-    print(points[-1])
-    print(steps)
+        print("location of found local minimum: ", np.round(points[-1], 2))
+        print("number of steps: ", steps)
+
 
 
 if __name__ == "__main__":
