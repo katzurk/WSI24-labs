545627775aea497739239a96ce2b5f0f4fd9fc3e
Katarzyna Kanicka <01187237@pw.edu.pl>
2025-01-06 10:58:22 +0100

    trained the model

--- lab7/main.py
+++ lab7/main.py
@@ -1,7 +1,9 @@
 import pandas as pd
-from pgmpy.estimators import PC, MaximumLikelihoodEstimator
+from pgmpy.estimators import PC, MaximumLikelihoodEstimator, HillClimbSearch, BicScore
 from pgmpy.models import BayesianNetwork
+from pgmpy.readwrite import XMLBIFWriter
 import seaborn as sns
+import multiprocessing
 from matplotlib import pyplot as plt
 import logging
 import warnings
@@ -13,7 +15,6 @@ def get_data():
     df = pd.read_csv("US_Crime_DataSet.csv")
     df = df[["Victim Sex", "Victim Age", "Victim Race", "Perpetrator Sex", "Perpetrator Age", "Perpetrator Race", "Relationship", "Weapon"]]
     df = df[~df.isin(['Unknown']).any(axis=1)]
-    df = df.head(100)
 
     for col in ["Victim Sex", "Victim Race", "Perpetrator Sex", "Perpetrator Race", "Relationship", "Weapon"]:
         df[col] = df[col].astype("category")
@@ -21,19 +22,27 @@ def get_data():
     df["Victim Age"] = pd.to_numeric(df["Victim Age"], errors="coerce")
     df["Perpetrator Age"] = pd.to_numeric(df["Perpetrator Age"], errors="coerce")
 
-    # Drop rows with any missing values after conversion
     df.dropna(inplace=True)
 
     return df
 
 data = get_data()
-print(data)
 
+# est = PC(data)
+# skel, separating_sets = est.build_skeleton(significance_level=0.05, max_cond_vars=2)
+# pdag = est.skeleton_to_pdag(skel, separating_sets)
+# dag = pdag.to_dag()
 
-est = PC(data)
-skel, seperating_sets = est.build_skeleton(significance_level=0.01)
-pdag = est.skeleton_to_pdag(skel, seperating_sets)
-dag = pdag.to_dag()
+hc = HillClimbSearch(data)
+best_model = hc.estimate(scoring_method=BicScore(data))
 
-model = BayesianNetwork(dag.edges())
+print(best_model.edges())
+
+model = BayesianNetwork(best_model.edges())
 model.fit(data, estimator=MaximumLikelihoodEstimator)
+
+for cpd in model.get_cpds():
+    print(cpd)
+
+# writer = XMLBIFWriter(model)
+# writer.write_xmlbif('BN_Model_US_Crime.xml')
