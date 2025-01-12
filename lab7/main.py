dcabdde61737248460c949d97290c89ec8f89622
Katarzyna Kanicka <01187237@pw.edu.pl>
2025-01-12 17:29:05 +0100

    tests

--- lab7/main.py
+++ lab7/main.py
@@ -2,8 +2,7 @@ import pandas as pd
 from pgmpy.estimators import PC, MaximumLikelihoodEstimator, HillClimbSearch, BicScore
 from pgmpy.models import BayesianNetwork
 from pgmpy.readwrite import XMLBIFWriter
-import seaborn as sns
-import multiprocessing
+from pgmpy.inference import VariableElimination
 from matplotlib import pyplot as plt
 import logging
 import warnings
@@ -13,8 +12,14 @@ logging.getLogger("pgmpy").setLevel(logging.WARNING)
 
 def get_data():
     df = pd.read_csv("US_Crime_DataSet.csv")
+    # df = df[(df["Year"] >= 1980) & (df["Year"] <= 2000)]
+    df.dropna()
+    df = df[~df.applymap(lambda x: x == 'Unknown').any(axis=1)]
+
     df = df[["Victim Sex", "Victim Age", "Victim Race", "Perpetrator Sex", "Perpetrator Age", "Perpetrator Race", "Relationship", "Weapon"]]
-    df = df[~df.isin(['Unknown']).any(axis=1)]
+
+    # filtered_df = df[(df['Victim Age'] == 998) & (df['Perpetrator Age'] == 0)]
+    # print(filtered_df)
 
     for col in ["Victim Sex", "Victim Race", "Perpetrator Sex", "Perpetrator Race", "Relationship", "Weapon"]:
         df[col] = df[col].astype("category")
@@ -22,27 +27,38 @@ def get_data():
     df["Victim Age"] = pd.to_numeric(df["Victim Age"], errors="coerce")
     df["Perpetrator Age"] = pd.to_numeric(df["Perpetrator Age"], errors="coerce")
 
-    df.dropna(inplace=True)
-
     return df
 
 data = get_data()
 
 # est = PC(data)
-# skel, separating_sets = est.build_skeleton(significance_level=0.05, max_cond_vars=2)
-# pdag = est.skeleton_to_pdag(skel, separating_sets)
+# dag = est.estimate(significance_level=0.01)
+# skel, seperating_sets = est.build_skeleton(significance_level=0.01)
+# pdag = est.skeleton_to_pdag(skel, seperating_sets)
 # dag = pdag.to_dag()
 
 hc = HillClimbSearch(data)
 best_model = hc.estimate(scoring_method=BicScore(data))
 
-print(best_model.edges())
+# print(dag.edges())
 
 model = BayesianNetwork(best_model.edges())
 model.fit(data, estimator=MaximumLikelihoodEstimator)
 
-for cpd in model.get_cpds():
-    print(cpd)
+# for cpd in model.get_cpds():
+#     print(cpd)
 
+# model_daft = model.to_daft()
+# # To open the plot
+# model_daft.render()
+# # Save the plot
+# model_daft.savefig('sachs.png')
 # writer = XMLBIFWriter(model)
 # writer.write_xmlbif('BN_Model_US_Crime.xml')
+
+infer = VariableElimination(model)
+q = infer.map_query(variables=['Victim Age', 'Victim Sex', 'Perpetrator Age', 'Relationship'], evidence={'Perpetrator Sex': 'Female'})
+print(q)
+
+graph = model.to_graphviz()
+graph.draw('model.png', prog='dot')
